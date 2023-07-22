190aba5a234079d492bf68207259af0a51b82c5e
Blake Taylor <sephnescence@gmail.com>
2023-07-22 12:29:24 +1000

    No actually this should

--- index.tsx
+++ index.tsx
@@ -6,5 +6,5 @@ const container = document.getElementById("root");
 if (container) {
     const root = createRoot(container);
     // root.render(<App />); // This will work as well. greeting prop is optional
-    root.render(<App greeting={"Sup"} />);
+    root.render(<App />);
 }
