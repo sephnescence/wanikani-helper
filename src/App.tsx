a0e96a241a33fcde47de6b36cb36b31eff8d0218
Blake Taylor <sephnescence@gmail.com>
2023-07-22 12:36:17 +1000

    Now?

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = (props: AppPropTypes) => {
   return (
     <div role="application">
       <h1>Hello world! I am using React</h1>
+      
       {props.greeting && <h2>{props.greeting}</h2>}
     </div>
   )
